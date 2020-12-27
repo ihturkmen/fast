678bb7a554277d2fc7df14c6ec37e5b6c8a18de3
İsmail Hakkı Türkmen <ihturkmen@gmail.com>
2020-12-27 19:40:33 +0300

    Add changeUrl, changeTitle option

---
+++ README.md
@@ -0,0 +1,46 @@
+## Fast - Load only what you need and shorten page load times (just like spa)
+
+Your project mimics the single page application working logic.
+Instead of calling the same resources over and over on each page while browsing through the site, you can save both time and traffic by calling only once when the project is opened.
+
+
+# Usage
+
+Just let me know the changed part of the page and for which links it will be active.
+
+```html
+<script>
+    new Fast({
+        fromSelector: "#pageContent",
+        toSelector: "#pageContent",
+        linkSelector: "a.redirectLink",
+        analytics: false,
+        changeUrl: true,
+        changeTitle: true,
+        onBefore: function(e) {
+            console.log(e);
+        },
+        onFinished: function(e) {
+            console.log(e);
+        },
+        onError: function(e) {
+            console.log(e);
+        },
+    });
+</script>
+```
+
+
+## Props
+
+| Option             | Type         | Description                                    |
+| ------------------ | ------------ | ---------------------------------------------- |
+| `fromSelector`     | String       |  |
+| `toSelector`       | String       |  |
+| `linkSelector`     | String       |  |
+| `analytics`        | Boolean      |  |
+| `changeUrl`        | Boolean      |  |
+| `changeTitle`      | Boolean      |  |
+| `onBefore`         | function     |  |
+| `onFinished`       | function     |  |
+| `onError`          | function     |  |
--- assets/js/fast.js
+++ assets/js/fast.js
@@ -1,74 +1,91 @@
+/*!
+ * Fast.js v1.0.0 - Fast Page Load
+ *
+ * Copyright (c) 2020 @ihturkmen
+ * All Rights Reserved. Apache Software License 2.0
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 (function() {
     this.Fast = function(prod) {
-        var botPattern = "(googlebot\/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
-        var re = new RegExp(botPattern, 'i');
-        var userAgent = navigator.userAgent; 
+        try {
+            if (!prod.linkSelector) {
+                throw "You must fill the linkSelector option";
+            }
+            if (!prod.fromSelector) {
+                throw "You must fill the fromSelector option";
+            }
+            if (!prod.toSelector) {
+                throw "You must fill the toSelector option";
+            }
+        } catch (err) {
+            throw err;
+        }
+
+        // Detect BOT
+        var botPattern = "(googlebot/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+        var re = new RegExp(botPattern, "i");
+        var userAgent = navigator.userAgent;
         if (re.test(userAgent)) {
             return false;
         }
-        var selector = document.querySelectorAll(prod.linkSelector);
-        
-        selector.forEach(function(e){
-            e.addEventListener("click",function(a){
-                const state = { }
-                const url = e.getAttribute('href');
 
-                fetch(url)
+        function init() {
+            var pageLink = document.querySelectorAll(prod.linkSelector);
+
+            pageLink.forEach(function(e) {
+                e.addEventListener("click", function(a) {
+                    callPage(e.getAttribute("href"));
+
+                    a.preventDefault();
+                    return false;
+                });
+            });
+        }
+
+        function callPage(link) {
+            if (prod.version) {
+                link = link + '?v=' + prod.version;
+            }
+
+            fetch(link)
                 .then(function(response) {
-                    return response.text()
+                    return response.text();
                 })
                 .then(function(html) {
                     var parser = new DOMParser();
                     var doc = parser.parseFromString(html, "text/html");
 
-                    //Get Title
-                    title = doc.head.querySelector("title").textContent;
-                    document.title = title;
-
                     //Get Content
-                    body = doc.body.querySelector(prod.targetSelector).innerHTML;
-                    document.querySelector(prod.localSelector).innerHTML = body;
+                    body = doc.body.querySelector(prod.fromSelector).innerHTML;
+                    document.querySelector(prod.fromSelector).innerHTML = body;
 
                     //Change URL
-                    history.pushState({}, title, url)
+                    if (prod.changeUrl != false) {
+                        history.pushState({}, title, link);
+                    }
+                    //Change Title
+                    if (prod.changeTitle != false) {
+                        var title = doc.head.querySelector("title").textContent;
+                        document.title = title;
+                    }
 
-                    if (prod.onOpened !== undefined) {
+                    if (prod.onOpened != undefined) {
                         prod.onOpened(e);
                     }
                 })
-                .catch(function(err) {  
-                    if (prod.onError !== undefined) {
-                        prod.onError(e);
+                .catch(function(err) {
+                    if (prod.onError != undefined) {
+                        prod.onError(err);
                     }
-                    console.log('Failed to fetch page: ', err);  
+                    console.log("Failed to fetch page: ", err);
                 });
 
-                if(prod.analytics){
-                    ga('send', 'pageview', url);
-                }
+            if (prod.analytics) {
+                ga("send", "pageview", link);
+            }
+        }
 
-                a.preventDefault(); //prevent default  behaviour
-                return false; //also return false
-            });
-        });    
-    }
-
-
-}());
-window.onpopstate = function(event) {
-    console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
-};
-
-new Fast({
-    localSelector:"#pageContent",
-    targetSelector:"#pageContent",
-    linkSelector:"a.redirectLink",
-    analytics:false,
-    onBefore:function(e){
-        console.log(e);
-    },
-    onFinished:function(e){
-        console.log(e);
-    },
-    //onError:function(e){alert("Error!");},
-});
+        return init();
+    };
+})();
